f8656ba041a4522a446ee0c01eb47122f0099503
Paul-Stelian Olaru <paul_stelian.olaru@stud.acs.upb.ro>
2019-09-10 22:19:39 +0300

    CNPVFS: Add interactive navigation to VFS

---
+++ CNPVFS.kt
@@ -0,0 +1,164 @@
+object CNPRootVFS : Directory {
+    override val entries: List<String>
+        get() {
+            val result = mutableListOf<String>()
+            // The root must support listing the normal objects
+            for (i in 0..9)
+                result.add(i.toString())
+            return result
+        }
+
+    override fun get(name: String): VFSEntry? {
+        // If the name is up to 12 characters and they're all digits, return a partialDirectory
+        if (name.length in 1..12 && name.all (Char::isDigit)) {
+            return PartialDirectory(name)
+        }
+        // If the name is exactly 13 characters and they're all digits, return a finalDirectory
+        if (name.length == 13 && name.all (Char::isDigit))
+            return FinalDirectory(name)
+        // If it's neither, just bail
+        return null
+    }
+
+    override fun toString(): String = "/"
+}
+
+class PartialDirectory(private val dirName: String): Directory {
+    override fun get(name: String): VFSEntry? {
+        if (name !in entries)
+            return null
+        if (name == "name")
+            return SimpleFile(dirName)
+        // We've got another partialDirectory, we shall make a symlink
+        return SimpleLink("/$dirName$name")
+    }
+
+    override val entries: List<String>
+        get() {
+            val result = mutableListOf<String>()
+            // Add the first result, "name"
+            result.add("name")
+            // Add symlinks to more-complete (by one digit) elements
+            for (i in 0..9)
+                result.add(i.toString())
+            return result
+        }
+
+    override fun toString(): String = "/$dirName"
+}
+
+class FinalDirectory(cnp: String): Directory {
+    private val cnp: Cnp = Cnp(cnp)
+    override fun get(name: String): VFSEntry? {
+        if (name !in entries)
+            return null
+        return when (name) {
+            "valid" -> SimpleFile(if (cnp.valid) "da" else "nu")
+            "nationalitate" -> SimpleFile(cnp.nationalitate)
+            "sex" -> SimpleFile(cnp.sex)
+            "an" -> SimpleFile(cnp.an.toString())
+            "luna" -> SimpleFile(cnp.luna.toString())
+            "numeLuna" -> SimpleFile(cnp.numeLuna)
+            "zi" -> SimpleFile(cnp.zi.toString())
+            "judet" -> SimpleFile(cnp.judet)
+            "judetNumeric" -> SimpleFile(cnp.judetNumeric.toString())
+            "nnn" -> SimpleFile(cnp.nnn.toString())
+            "name" -> SimpleFile(cnp.name)
+            "schimba" -> ChangesList(cnp)
+            else -> null
+        }
+    }
+
+    override val entries: List<String>
+        get() {
+            val result = mutableListOf<String>()
+            // Add a few classes
+            result.add("valid")
+            if (!cnp.valid)
+                return result
+            result.add("nationalitate")
+            result.add("sex")
+            result.add("an")
+            result.add("luna")
+            result.add("numeLuna")
+            result.add("zi")
+            result.add("judet")
+            result.add("judetNumeric")
+            result.add("nnn")
+            result.add("name")
+            // allow changes
+            result.add("schimba")
+            return result
+        }
+
+    override fun toString(): String = "/$cnp"
+}
+
+class SimpleFile(override val contents: String): File {
+    override fun toString(): String = contents
+}
+class SimpleLink(override val target: String): SymLink {
+    override fun toString(): String = "[Symlink -> $target]"
+}
+
+class ChangesList(private val cnp: Cnp): VFSEntry, Directory {
+    override fun get(name: String): VFSEntry? {
+        return when (name) {
+            "nationalitate" -> NationalitiesList(cnp)
+            "sex" -> SexList(cnp)
+            "an" -> YearList(cnp)
+            "luna" -> MonthList(cnp)
+            "zi" -> DayList(cnp)
+            "judet" -> CountyList(cnp)
+            "nnn" -> NNNList(cnp)
+            else -> null
+        }
+    }
+
+    override val entries: List<String>
+        get() {
+            return listOf("nationalitate", "sex", "an", "luna", "zi", "judet", "nnn")
+        }
+
+    override fun toString(): String = "/$cnp/schimba"
+}
+
+class NationalitiesList(private val cnp: Cnp): Directory {
+    override fun get(name: String): VFSEntry? = if (name in entries) SimpleLink("/" + cnp.altaNationalitate(name).name) else null
+    override val entries: List<String> get() = cnp.nationalitatiValide
+    override fun toString(): String = "/$cnp/schimba/nationalitate"
+}
+class SexList(private val cnp: Cnp): Directory {
+    override fun get(name: String): VFSEntry? = if (name in entries) SimpleLink("/" + cnp.altSex(name).name) else null
+    override val entries: List<String> get() = cnp.sexeValide
+    override fun toString(): String = "/$cnp/schimba/sex"
+}
+class YearList(private val cnp: Cnp): Directory {
+    override fun get(name: String): VFSEntry? = if (name in entries) get(name.toInt()) else null
+    fun get(year: Int): VFSEntry = SimpleLink("/" + cnp.altAn(year).name)
+    override val entries: List<String> get() = cnp.aniValizi.map (Int::toString)
+    override fun toString(): String = "/$cnp/schimba/an"
+}
+class MonthList(private val cnp: Cnp): Directory {
+    override fun get(name: String): VFSEntry? = if (name in entries) get(name.toInt()) else null
+    fun get(month: Int): VFSEntry = SimpleLink("/" + cnp.altaLuna(month).name)
+    override val entries: List<String> get() = (1..12).map(Int::toString)
+    override fun toString(): String = "/$cnp/schimba/luna"
+}
+class DayList(private val cnp: Cnp): Directory {
+    override fun get(name: String): VFSEntry? = if (name in entries) get(name.toInt()) else null
+    fun get(day: Int): VFSEntry = SimpleLink("/" + cnp.altaZi(day).name)
+    override val entries: List<String> get() = cnp.zileValide.map(Int::toString)
+    override fun toString(): String = "/$cnp/schimba/zi"
+}
+class CountyList(private val cnp: Cnp): Directory {
+    override fun get(name: String): VFSEntry? = if (name in entries) SimpleLink("/" + cnp.altJudetGeneral(name).name) else null
+    override val entries: List<String> get() = cnp.judeteGeneraleValide
+    override fun toString(): String = "/$cnp/schimba/judet"
+}
+class NNNList(private val cnp: Cnp): Directory {
+    override fun get(name: String): VFSEntry? = if (name in entries) get(name.toInt()) else null
+    fun get(nnn: Int): VFSEntry = SimpleLink("/" + cnp.altNnn(nnn).toString())
+    override val entries: List<String> get() = (0..999).map { it.toString().padStart(3, '0') }
+    override fun toString(): String = "/$cnp/schimba/nnn"
+}
--- main.kt
+++ main.kt
@@ -1,11 +1,6 @@
-sealed class CNPVFSEntry {
-    open val resolved: CNPVFSEntry? get() = this
-
+interface VFSEntry {
     companion object {
-        val root = RootVFS
-        fun resolve(name: String): CNPVFSEntry? = root[name]
-        fun resolve(entry: CNPVFSEntry?): CNPVFSEntry? =
-            if (entry is SymLink) entry.resolved else entry
+        val cnpRoot = CNPRootVFS
     }
 
     val type: String get() = when (this) {
@@ -16,203 +11,134 @@ sealed class CNPVFSEntry {
     }
 }
 
-interface Directory {
-    operator fun get(name: String): CNPVFSEntry?
-    val entries: List<String> get
+interface Directory: VFSEntry {
+    operator fun get(name: String): VFSEntry?
+    val entries: List<String>
 }
 
-interface File {
-    val contents: String get
+interface File: VFSEntry {
+    val contents: String
 }
 
-interface SymLink {
-    val target: String get
-    val resolved: CNPVFSEntry? get() = CNPVFSEntry.resolve(target)
+interface SymLink: VFSEntry {
+    val target: String
 }
 
-object RootVFS : CNPVFSEntry(), Directory {
-    override val entries: List<String>
-        get() {
-            val result = mutableListOf<String>()
-            // The root must support listing the normal objects
-            for (i in 0..9)
-                result.add(i.toString())
-            return result
-        }
-
-    override fun get(name: String): CNPVFSEntry? {
-        // If the name is up to 12 characters and they're all digits, return a partialDirectory
-        if (name.length in 1..12 && name.all (Char::isDigit)) {
-            return PartialDirectory(name)
-        }
-        // If the name is exactly 13 characters and they're all digits, return a finalDirectory
-        if (name.length == 13 && name.all (Char::isDigit))
-            return FinalDirectory(name)
-        // If it's neither, just bail
-        return null
-    }
-}
-
-class PartialDirectory(val dirName: String): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? {
-        if (name !in entries)
-            return null
-        if (name == "name")
-            return SimpleFile(dirName);
-        // We've got another partialDirectory, we shall make a symlink
-        return SimpleLink(dirName + name)
-    }
-
-    override val entries: List<String>
-        get() {
-            val result = mutableListOf<String>()
-            // Add the first result, "name"
-            result.add("name")
-            // Add symlinks to more-complete (by one digit) elements
-            for (i in 0..9)
-                result.add(i.toString())
-            return result
-        }
-}
-
-class FinalDirectory(cnp: String): CNPVFSEntry(), Directory {
-    private val cnp: Cnp = Cnp(cnp)
-    override fun get(name: String): CNPVFSEntry? {
-        if (name !in entries)
-            return null
+class DirectoryWithParent(private val parentDirectory: DirectoryWithParent?, private val curDir: Directory, private val name: String) {
+    operator fun get(name: String): DirectoryWithParent? {
+        val child = curDir[name] as? Directory
         return when (name) {
-            "valid" -> SimpleFile(if (cnp.valid) "da" else "nu")
-            "nationalitate" -> SimpleFile(cnp.nationalitate)
-            "sex" -> SimpleFile(cnp.sex)
-            "an" -> SimpleFile(cnp.an.toString())
-            "luna" -> SimpleFile(cnp.luna.toString())
-            "numeLuna" -> SimpleFile(cnp.numeLuna)
-            "zi" -> SimpleFile(cnp.zi.toString())
-            "judet" -> SimpleFile(cnp.judet)
-            "judetNumeric" -> SimpleFile(cnp.judetNumeric.toString())
-            "nnn" -> SimpleFile(cnp.nnn.toString())
-            "name" -> SimpleFile(cnp.name)
-            "schimba" -> ChangesList(cnp)
-            else -> null
+            "", "." -> this
+            ".." -> parentDirectory
+            else -> DirectoryWithParent(this, child ?: return null, name)
         }
     }
-
-    override val entries: List<String>
-        get() {
-            val result = mutableListOf<String>()
-            // Add a few classes
-            result.add("valid")
-            if (!cnp.valid)
-                return result
-            result.add("nationalitate")
-            result.add("sex")
-            result.add("an")
-            result.add("luna")
-            result.add("numeLuna")
-            result.add("zi")
-            result.add("judet")
-            result.add("judetNumeric")
-            result.add("nnn")
-            result.add("name")
-            // allow changes
-            result.add("schimba")
-            return result
-        }
-}
-
-class SimpleFile(override val contents: String): CNPVFSEntry(), File
-class SimpleLink(override val target: String): CNPVFSEntry(), SymLink {
-    override val resolved get() = super<SymLink>.resolved
-}
-
-class ChangesList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? {
-        return when (name) {
-            "nationalitate" -> NationalitiesList(cnp)
-            "sex" -> SexList(cnp)
-            "an" -> YearList(cnp)
-            "luna" -> MonthList(cnp)
-            "zi" -> DayList(cnp)
-            "judet" -> CountyList(cnp)
-            "nnn" -> NNNList(cnp)
-            else -> null
-        }
+    operator fun invoke(): Directory = curDir
+    override fun toString(): String = if (parentDirectory != null) "$parentDirectory/$name" else "/$name"
+    companion object {
+        val cnpRoot = DirectoryWithParent(null, VFSEntry.cnpRoot, "")
     }
+}
 
-    override val entries: List<String>
-        get() {
-            return listOf("nationalitate", "sex", "an", "luna", "zi", "judet", "nnn")
+inline class DirectoryWithSymlinkResolution(private val underlying: DirectoryWithParent) {
+    operator fun get(name: String): DirectoryWithSymlinkResolution? {
+        // We only resolve to a directory so this is fine
+        if (name == "")
+            return this
+        if (!name.contains('/')) {
+            when (name) {
+                // Simpler cases
+                "", "." -> return this
+                // ".." -> works fine with below case
+            }
+            // Simple resolution, just resolve one path entry
+            val child = if (name == "..") this()[name]!!() else this()()[name] ?: return null
+            if (child is Directory) {
+                return DirectoryWithSymlinkResolution(underlying[name]!!)
+            }
+            if (child is SymLink) {
+                val result = this[child.target] ?: return null
+                // It's a directory but with a wrong parent now
+                val unpackedResult = result()()
+                val reparentedResult = DirectoryWithParent(this(), unpackedResult, name)
+                return DirectoryWithSymlinkResolution(reparentedResult)
+            }
+            return null
         }
-
+        if (name[0] == '/')
+            return cnpRoot[name.substring(1)]
+        // Parse the first component up to the target
+        val currentName = name.substringBefore("/")
+        val remainder = name.substringAfter("/")
+        val currentResolved = this[currentName] ?: return null
+        return currentResolved[remainder]
+    }
+    operator fun invoke() = underlying
+    override fun toString(): String = underlying.toString()
+    companion object {
+        val cnpRoot = DirectoryWithSymlinkResolution(DirectoryWithParent.cnpRoot)
+    }
 }
 
-class NationalitiesList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? = if (name in entries) SimpleLink(cnp.altaNationalitate(name).name) else null
-    override val entries: List<String> get() = cnp.nationalitatiValide
+fun interactiveListCurrent(curDir: DirectoryWithSymlinkResolution) {
+    // Must get all elements in here
+    for (file in curDir()().entries)
+        println(file)
 }
-class SexList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? = if (name in entries) SimpleLink(cnp.altSex(name).name) else null
-    override val entries: List<String> get() = cnp.sexeValide
+fun interactiveListChild(curDir: DirectoryWithSymlinkResolution, target: String) {
+    val child = curDir()()[target]
+    if (child == null) {
+        println("Error: No such file or directory $target")
+        return
+    }
+    if (child !is Directory) {
+        println(target)
+        return
+    }
+    // Since we know that the child is a directory, we can just do the below
+    interactiveListCurrent(curDir[target]!!)
 }
-class YearList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? = if (name in entries) get(name.toInt()) else null
-    fun get(year: Int): CNPVFSEntry = SimpleLink(cnp.altAn(year).name)
-    override val entries: List<String> get() = cnp.aniValizi.map (Int::toString)
-}
-class MonthList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? = if (name in entries) get(name.toInt()) else null
-    fun get(month: Int): CNPVFSEntry = SimpleLink(cnp.altaLuna(month).name)
-    override val entries: List<String> get() = (1..12).map(Int::toString)
-}
-class DayList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? = if (name in entries) get(name.toInt()) else null
-    fun get(day: Int): CNPVFSEntry = SimpleLink(cnp.altaZi(day).name)
-    override val entries: List<String> get() = cnp.zileValide.map(Int::toString)
-}
-class CountyList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? = if (name in entries) SimpleLink(cnp.altJudetGeneral(name).name) else null
-    override val entries: List<String> get() = cnp.judeteGeneraleValide
-}
-class NNNList(val cnp: Cnp): CNPVFSEntry(), Directory {
-    override fun get(name: String): CNPVFSEntry? = if (name in entries) get(name.toInt()) else null
-    fun get(nnn: Int): CNPVFSEntry = SimpleLink(cnp.altNnn(nnn).toString())
-    override val entries: List<String> get() = (0..999).map { it.toString().padStart(3, '0') }
-}
-
-fun lsRecursive(dir: Directory, prefix: String = "") {
-    for (entry in dir.entries) {
-        val elem = dir[entry]
-        val type = elem?.type ?: "<NOT FOUND>"
-        val symlinklore = if (elem is SymLink) {
-            val targetElem = elem.resolved
-            val targetName = elem.target
-            val targetType = targetElem?.type ?: "<NOT FOUND>"
-            "(symlink to /$targetName: $targetType)"
-        } else ""
-        println("$prefix$entry: $type$symlinklore")
-        if (elem is Directory)
-            lsRecursive(elem, "$prefix\t")
-        if (elem is File)
-            println("$prefix\tFile contents: \"${elem.contents}\"")
+fun interactivePrintFile(curDir: DirectoryWithSymlinkResolution, target: String) {
+    when (val entry = curDir()()[target]) {
+        null -> println("Error: No such file or directory $target")
+        !is File -> println("Error: $target is not a file")
+        else -> println(entry.contents)
     }
 }
 
 fun main() {
-    val root = CNPVFSEntry.root
-    val partial = root["100010101000"]
-    if (partial == null) {
-        println("/100010101000 not found")
-        return
+    var curDir = DirectoryWithSymlinkResolution.cnpRoot
+    val commandRegex = Regex("""\s*(ls|cd|cat)\s+(.*)""")
+
+    interactiveLoop@ while (true) {
+        print("> ")
+        val command = readLine() ?: return
+        when (command) {
+            "" -> continue@interactiveLoop
+            "exit", "quit" -> return
+            "ls" -> {interactiveListCurrent(curDir); continue@interactiveLoop}
+            "pwd" -> {
+                println(curDir)
+                continue@interactiveLoop
+            }
+        }
+        val match = commandRegex.matchEntire(command)
+        if (match == null) {
+            println("Invalid command!")
+            continue@interactiveLoop
+        }
+        val (commandName, argument) = match.destructured
+        when (commandName) {
+            "ls" -> interactiveListChild(curDir, argument)
+            "cd" -> {
+                val target = curDir[argument]
+                if (target == null) {
+                    println("Cannot change to directory $argument")
+                } else {
+                    curDir = target
+                }
+            }
+            "cat" -> interactivePrintFile(curDir, argument)
+        }
     }
-    if (partial !is Directory) {
-        println("/100010101000 is not a directory")
-        return
-    }
-    val final = partial["6"]?.resolved
-    if (final !is Directory) {
-        println("/100010101000/6 is not a directory")
-        return
-    }
-    println("Listing of /1000101010006: ${final.entries}")
-    lsRecursive(final)
 }
